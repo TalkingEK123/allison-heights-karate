f4749a2ff567838e0ce2052b5d145abea6ba115d
YouIsm00813 <eyi1901@umoncton.ca>
2025-08-23 00:56:01 -0300

    website hosting stuff

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://allisonheightskarate.ca',
+  generateRobotsTxt: true, // also generates /robots.txt
+  // optional nice-to-haves:
+  // changefreq: 'weekly',
+  // priority: 0.7,
+  // exclude: ['/404', '/500'],
+};
